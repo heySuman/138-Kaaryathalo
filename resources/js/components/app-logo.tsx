20a06d5e18f305ca8fbd65465c9b3e49a897fe5d
heySuman <iamsuman066@gmail.com>
2025-03-14 23:38:29 +0545

    feat: authentication with starter pack modified to match the system requirement

---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,14 @@
+import AppLogoIcon from './app-logo-icon';
+
+export default function AppLogo() {
+    return (
+        <>
+            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+                <span className="size-5 fill-current text-white dark:text-black font-bold dark:font-bold">K</span>
+            </div>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-none font-semibold">Kaaryathalo</span>
+            </div>
+        </>
+    );
+}
